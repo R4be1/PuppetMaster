2dca67cec46249dffe2282e3409d65a418a0bfe8
Rebel. <110738599+R4be1@users.noreply.github.com>
2024-08-08 04:04:35 +0800

    Update Master.py

--- Master.py
+++ Master.py
@@ -160,7 +160,7 @@ async def handle_shell_init(reader, writer):
                     username = "<unknown>"
 
                 #获取CPU核心数
-                session_cpu_core = str(cpuinfo.count("processor"))
+                session_cpu_core = str(cpuinfo.count("\nprocessor"))
                 #根据系统信息计算Session Hash
                 session_hash = hashlib.md5(puppetHash.encode()).hexdigest()
 
